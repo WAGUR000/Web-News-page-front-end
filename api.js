8d5969be277f604c607eaab7101fd256b9308972
장민혁 <wkdalsgur000@gmail.com>
2025-11-28 16:42:46 +0900

    f

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const BASE_URL = `https://xxterco9tj.execute-api.ap-northeast-2.amazonaws.com/default/Access_DynamoDB`;
+// Reader Lambda API URL (분석용)
 const ANALYTICS_API_URL = "https://xxterco9tj.execute-api.ap-northeast-2.amazonaws.com/default/Request_Analysis_Lambda"; 
 
 async function handleApiResponse(response) {
@@ -10,14 +11,17 @@ async function handleApiResponse(response) {
     return response.json();
 }
 
+// 메인 페이지 뉴스 (기존 유지)
 export async function fetchMainPageNews(dateString) {
     const url = `${BASE_URL}?mode=all_categories_summary&date=${dateString}`;
     const response = await fetch(url);
     return handleApiResponse(response);
 }
 
+// [수정됨] 뉴스 탐색 (400 에러 해결 시도: category_list -> list)
 export async function fetchExploreNews({ category, sortBy, limit, date, exclusiveStartKey }) {
-    let url = `${BASE_URL}?mode=category_list&category=${encodeURIComponent(category)}&sortBy=${sortBy}&limit=${limit}`;
+    // mode를 'list'로 변경하여 호출합니다. (백엔드 스펙에 따라 'news' 또는 'category'일 수도 있습니다)
+    let url = `${BASE_URL}?mode=list&category=${encodeURIComponent(category)}&sortBy=${sortBy}&limit=${limit}`;
     
     if (date) url += `&date=${date}`;
     if (exclusiveStartKey) {
@@ -29,7 +33,7 @@ export async function fetchExploreNews({ category, sortBy, limit, date, exclusiv
     return handleApiResponse(response);
 }
 
-// [추가] 분석 데이터 가져오기
+// [신규] 분석 데이터 호출
 export async function fetchAnalyticsData() {
     const response = await fetch(ANALYTICS_API_URL);
     return handleApiResponse(response);
